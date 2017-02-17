34df2388fd7fd487107037f2d093e32cc7450eea
Staehler, Michael <michael.staehler@opitz-consulting.com>
2017-02-17 16:12:14 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroesComponent} from "./heroes.component";
+import {HeroService} from "./hero.service";
+import {DashboardComponent} from "./dashboard.component";
+import {AppRoutingModule} from "./app-routing.module";
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        HttpModule,
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        DashboardComponent
+    ],
+    providers: [
+        HeroService
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+
+export class AppModule {
+}
